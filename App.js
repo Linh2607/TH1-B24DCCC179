cc8b280656d5159ce4daecc2acb62ee7baf70ff0
B24DCCC179-Nguyễn Vũ Việt Linh <linh5419791@gmail.com>
2025-09-29 11:36:53 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import TextCounter from './TextCounter';
+
+function App() {
+  return (
+    <div>
+      <h1>Demo TextCounter Component</h1>
+      <TextCounter />
+    </div>
+  );
+}
+
+export default App;
